d20baec8a798251386f487c61114f421dbb53df2
ritesh9067 <riteshsonawane182003@gmail.com>
2024-05-09 12:25:41 +0530

    SumOfNumbers.java

---
+++ SumOfNumbers.java
@@ -0,0 +1,18 @@
+
+import java.io.*; 
+
+class SumOfNumbers { 
+	public static void main(String[] args) 
+	{ 
+		int N = 10; 
+		int sum = 0; 
+		System.out.println("Finding Sum of " + N + " numbers using for loop"); 
+
+		for (int i = 1; i <= N; i++) { 
+			sum += i; 
+		} 
+		
+		System.out.println("Sum of first " + N 
+						+ " Natural Number = " + sum); 
+	} 
+}
